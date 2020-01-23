c9b98fef6f8789751666b174a5495bdf40c6cf46
Tom Henderson <tomhenderson@mac.com>
2020-01-24 09:54:26 +1300

    Add template

---
+++ Spoons/Template.spoon/init.lua
@@ -0,0 +1,11 @@
+local obj = {}
+obj.__index = obj
+
+-- Metadata
+obj.name = "Template"
+obj.version = "1.0"
+obj.author = "Tom Henderson <tomhenderson@mac.com>"
+obj.homepage = "https://tom-henderson.github.io"
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+return obj
