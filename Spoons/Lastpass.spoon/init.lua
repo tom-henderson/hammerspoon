562436cbcc2f9c986c1b04fa515cdf0427dda7f9
Tom Henderson <tomhenderson@mac.com>
2020-01-31 14:17:59 +1300

    Refactor a bit

--- Spoons/Lastpass.spoon/init.lua
+++ Spoons/Lastpass.spoon/init.lua
@@ -15,18 +15,6 @@ local function script_path()
 end
 obj.spoonPath = script_path()
 
-obj.choices = {}
-obj.chooser = hs.chooser.new(function(choice)
-    if not choice then return end
-    print(choice["text"])
-    hs.task.new("/usr/local/bin/lpass", function() return true end, {"show", "--clip", "--password", choice["id"]}):start()
-end)
-
-obj.chooser:width(30)
-obj.chooser:rows(8)
-
-hs.hotkey.bind({"ctrl"}, "Space", function() obj.chooser:show() end)
-
 function obj.parse_lpass(task, stdOut, stdErr)
     -- print("Lodader finished " .. exitCode)
     if (task == nil) then
@@ -58,22 +46,57 @@ function obj.parse_lpass(task, stdOut, stdErr)
     return true
 end
 
-function obj.load_complete(exitCode, stdOut, stdErr)
-    print(exitCode)
-    hs.notify.new({title="Hammerspoon", informativeText="Lastpass Loaded"}):send()
+function obj.copy_password(item)
+    if not choice then return end
+    -- print(choice["text"])
+    hs.task.new("/usr/local/bin/lpass", function() return true end, {"show", "--clip", "--password", choice["id"]}):start()
 end
 
 function obj.reload()
     print("Reloading items")
-    hs.task.new("/usr/local/bin/lpass", obj.load_complete, obj.parse_lpass, {"ls", "--color", "never", "--format", "%ai|%/as%/ag%an"}):start()
+    hs.task.new("/usr/local/bin/lpass", function()
+        hs.notify.new({title="Lastpass", informativeText="Vault loaded."}):send()
+    end, obj.parse_lpass, {"ls", "--color", "never", "--format", "%ai|%/as%/ag%an"}):start()
 end
 
+function obj.generate_password()
+    return true
+end
+
+function obj.lock()
+    obj.choices = {}
+    obj.chooser:choices(obj.choices)
+    hs.task.new("/usr/local/bin/lpass", function() 
+        hs.notify.new({title="Lastpass", informativeText="Vault locked."}):send()
+    end, {"logout", "--force"}):start()
+end
+
+-- Chooser
+obj.choices = {}
+obj.chooser = hs.chooser.new(obj.copy_password)
+obj.chooser:width(30)
+obj.chooser:rows(8)
+
+obj.chooser:showCallback(function()
+    if (#obj.choices == 0) then
+        obj.reload()
+    end
+    return obj.chooser
+end)
+
+-- Menu
 obj.menu = hs.menubar.new()
 obj.menu:setIcon(obj.spoonPath.."/lastpass.tiff", true) 
 obj.menu:setMenu(
     {
+        { title = "Quick Search", fn = obj.chooser.show },
+        { title = "Generate Password", fn = obj.generate_password },
+        { title = "-" },
         { title = "Refresh", fn = obj.reload },
+        { title = "Lock Lastpass", fn = obj.lock },
     }
 )
+
+hs.hotkey.bind({"ctrl"}, "Space", function() obj.chooser:show() end)
 
 return obj
