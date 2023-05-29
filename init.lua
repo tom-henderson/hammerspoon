360e60bed35244b8a404a9f797fbf1cd3d52c9ce
Tom Henderson <tomhenderson@mac.com>
2023-05-29 14:32:15 +1200

    :lipstick:

--- init.lua
+++ init.lua
@@ -7,7 +7,6 @@ hs.loadSpoon("Caffiene")
 hs.loadSpoon("SizeUp")
 hs.loadSpoon("WiFi")
 hs.loadSpoon("ScreenSaver")
--- hs.loadSpoon("ColourPicker")
 hs.loadSpoon("LastPass")
 hs.loadSpoon("HomeAssistant")
 hs.loadSpoon("Targus")
@@ -16,21 +15,3 @@ hs.hotkey.bind({"shift", "cmd"}, ",", function()
     hs.application.launchOrFocus("System Preferences")
 end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "c", function()
-    hs.dialog.color.show()
-end)
-
--- menu = hs.menubar.new()
--- function updateMenuTitle(code, body, headers)
---     json_response = hs.json.decode(body)
---     temperature = json_response["data"]["result"][1]["value"][2]
---     menu:setTitle(tostring(temperature).."℃")
--- end
-
--- function getTemperature()
---     hs.http.doAsyncRequest('http://10.0.0.5:9090/api/v1/query?query='..query, 'GET', nil, nil, updateMenuTitle)
--- end
-
--- query = hs.http.encodeForQuery('temperature{room="office"}')
--- getTemperature()
--- menu:setClickCallback(getTemperature)
