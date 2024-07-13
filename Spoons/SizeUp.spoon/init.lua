47b762f933762ad29a2ebcaf309547165b7bcdb4
Tom Henderson <tomhenderson@mac.com>
2024-07-13 21:16:17 +1200

    Allow repeating keypresses

--- Spoons/SizeUp.spoon/init.lua
+++ Spoons/SizeUp.spoon/init.lua
@@ -55,24 +55,22 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "return", function() hs.hints.windowHints
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad*", function() hs.hints.windowHints() end)
 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M",    function() obj.set_window_location(obj.positions.max) end)
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "C",    function() obj.set_window_location(obj.positions.center) end)
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "I",    function() obj.set_window_location(obj.positions.center_full_height) end)
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "-",    function() obj.set_window_location(obj.positions.center_full_width) end)
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad5", function() obj.set_window_location(obj.positions.center) end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function() obj.set_window_location(obj.positions.left) end)
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad4", function() obj.set_window_location(obj.positions.left) end)
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right",function() obj.set_window_location(obj.positions.right) end)
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad6", function() obj.set_window_location(obj.positions.right) end)
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up",   function() obj.set_window_location(obj.positions.top) end)
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad8", function() obj.set_window_location(obj.positions.top) end)
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function() obj.set_window_location(obj.positions.bottom) end)
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad2", function() obj.set_window_location(obj.positions.bottom) end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad7", function() obj.set_window_location(obj.positions.top_left) end)
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad9", function() obj.set_window_location(obj.positions.top_right) end)
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad1", function() obj.set_window_location(obj.positions.bottom_left) end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad2", function() obj.set_window_location(obj.positions.bottom) end)
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad3", function() obj.set_window_location(obj.positions.bottom_right) end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad4", function() obj.set_window_location(obj.positions.left) end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad5", function() obj.set_window_location(obj.positions.center) end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad6", function() obj.set_window_location(obj.positions.right) end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad7", function() obj.set_window_location(obj.positions.top_left) end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad8", function() obj.set_window_location(obj.positions.top) end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "pad9", function() obj.set_window_location(obj.positions.top_right) end)
 
 
 -- Next Screen
@@ -110,5 +108,65 @@ hs.hotkey.bind({"alt", "ctrl", "shift", "cmd"}, "Left", function()
 
     win:moveOneScreenWest(false, true, obj.animationDuration)
 end)
+
+function position_hotkey(mods, key, positions, timeout)
+    local this = {}
+    this._timeout = timeout or 3
+    this._counter = 1
+    this._timer = nil
+    this.modal =  hs.hotkey.modal.new(mods, key)
+
+    function this.move_window(positions)
+        local win =  hs.window.focusedWindow()
+        win:moveToUnit(positions[this._counter], obj.animationDuration)
+        this._counter = this._counter + 1
+        if this._counter > #positions then
+            this._counter = 1
+        end
+    end
+
+    this.modal:bind(mods, key, function()
+        this._timer:setDelay(this._timeout)
+        this.move_window(positions)
+    end)
+
+    function this.modal:entered()
+        local win =  hs.window.focusedWindow()
+        if not obj.snapback_window_state[win:id()] then
+            obj.snapback_window_state[win:id()] = win:frame()
+        end
+        this._timer = hs.timer.delayed.new(this._timeout, function() this.modal:exit() end):start()
+        this.move_window(positions)
+    end
+
+    function this.modal:exited()
+        this._counter = 1
+    end
+
+    return this
+end
+
+function obj.center_position(w, h)
+    return {x=(0.5 - w/2), y=(0.5 - h/2), w=w, h=h}
+end
+
+obj.center = position_hotkey({"cmd", "alt", "ctrl"}, "C", {
+    obj.center_position(0.5, 0.75),
+    obj.center_position(0.6, 0.85),
+    obj.center_position(0.7, 0.95),
+    obj.center_position(0.8, 0.95),
+})
+obj.left = position_hotkey({"cmd", "alt", "ctrl"}, "Left", {
+    hs.layout.left50,
+    hs.layout.left30,
+    hs.layout.left25,
+    hs.layout.left70,
+})
+obj.right = position_hotkey({"cmd", "alt", "ctrl"}, "Right", {
+    hs.layout.right50,
+    hs.layout.right30,
+    hs.layout.right25,
+    hs.layout.right70,
+})
 
 return obj
